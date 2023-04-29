ce7514f7348f7b450a1a050e76def9221b94410d
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2023-04-28 23:35:27 -0300

    Tratamento de excessão, criação da classe AppError

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,6 +1,7 @@
-import express from 'express'
+import express, { NextFunction, Response, Request } from 'express'
 import { router } from './routes'
 import dotenv from 'dotenv'
+import { AppError } from '../../errors/AppError'
 
 dotenv.config()
 
@@ -9,5 +10,18 @@ const app = express()
 app.use(express.json())
 
 app.use(router)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof AppError){
+        return response.status(err.statusCode).json({
+            message: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: `Internal server error - ${err.message}`
+    })
+})
 
 export { app }
