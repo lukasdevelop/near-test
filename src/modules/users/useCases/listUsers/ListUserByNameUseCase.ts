1cbe8df26d1b708383cc2457fbc62016d72df4ec
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2023-04-29 05:30:52 -0300

    TsConfig para decorators e class de ListUserByNameUseCase

--- src/modules/users/useCases/listUsers/ListUserByNameUseCase.ts
+++ src/modules/users/useCases/listUsers/ListUserByNameUseCase.ts
@@ -1,6 +1,18 @@
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { User } from "modules/users/entities/User";
 
+
+@injectable()
 class ListUsersByNameUseCase {
+  constructor(
+    @inject("UsersRepository") private usersRepository: IUsersRepository
+  ) {}
 
+  async execute(name: string): Promise<User>{
+    const users = await this.usersRepository.findByName(name)
+    return users
+  }
 }
 
-export { ListUsersByNameUseCase }
+export { ListUsersByNameUseCase };
