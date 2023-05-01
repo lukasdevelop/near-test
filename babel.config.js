842800ccda142867fd463eb66727c13ebc51afe8
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2023-05-01 16:24:22 -0300

    Configuração do Babel para build

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@modules": ["./src/modules"],
+          "@services": ["./src/services"],
+          "@shared": ["./src/shared"],
+          "@errors": ["./src/errors"],
+          "@helpers": ["./src/helpers"],
+        },
+      },
+    ],
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+  ],
+};
